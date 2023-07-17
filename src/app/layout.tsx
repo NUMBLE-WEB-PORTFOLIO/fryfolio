2c5425ffc738013e2bec4de615b8659cdb1b105b
gxxrxn <rbfks1221@naver.com>
2023-07-17 15:14:47 +0900

    refactor: header export 방식 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import { Header } from "@/components/Header";
+import Header from "@/components/Header";
 
 import "@/styles/globals.css";
 
