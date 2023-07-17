648ede9d57e7505519436cc2349a26912218187c
gxxrxn <rbfks1221@naver.com>
2023-07-17 20:03:42 +0900

    refactor: 배경 위치 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-main-light ">
       <section id="home-interaction" className="h-screen z-10">
-        <div className="absolute top-0 left-0 w-full h-screen">
+        <div className="absolute top-0 left-0 bottom-[1rem] w-full h-srceen overflow-hidden">
           <Image
-            src="/images/eggline-cropped.svg"
-            alt="계란후라이 테두리"
+            src="/images/eggline.svg"
+            alt="background"
+            objectFit="cover"
+            objectPosition="calc(50% - 2rem) bottom"
+            className="min-w-[104%]"
             fill
+            priority
           />
         </div>
         <div className="absolute top-[40%] left-[10%]">
