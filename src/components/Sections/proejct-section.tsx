e303128d78f97818ec222a150015f6e3b9e61377
gxxrxn <rbfks1221@naver.com>
2023-10-27 22:48:11 +0900

    feat: dadok project card 구현

---
+++ src/components/Sections/proejct-section.tsx
@@ -0,0 +1,11 @@
+import ProjectCardDadok from "./projects/project-card-dadok";
+
+const ProjectSection = () => {
+  return (
+    <section className="bg-main-dark">
+      <ProjectCardDadok />
+    </section>
+  );
+};
+
+export default ProjectSection;
