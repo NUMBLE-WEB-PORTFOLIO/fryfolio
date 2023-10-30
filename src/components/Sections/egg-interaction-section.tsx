431ebba32e1b12c0f409170ecb879d34d8652e17
gxxrxn <rbfks1221@naver.com>
2023-10-30 17:31:02 +0900

    fix: egg interaction section bgImage none to cover

--- src/components/Sections/egg-interaction-section.tsx
+++ src/components/Sections/egg-interaction-section.tsx
@@ -4,13 +4,13 @@ import { EggYork as AnimatedEggYork, BounceBox } from "@/components/Motion";
 
 const EggInteractionSection = () => {
   return (
-    <section className="w-full h-full overflow-hidden flex justify-center relative">
+    <section className="w-full h-full overflow-hidden flex justify-center relative min-h-[100dvh]">
       <div className="absolute top-0 left-0 bottom-0 right-0 bg-[#fff]"></div>
       <div className="absolute top-[-10px] left-0 right-0 bottom-0 w-full h-srceen overflow-hidden z-10">
         <Image
           src="/images/eggline-filled.svg"
           alt="background egg"
-          className="relative sm:scale-[104%] overflow-hidden object-none"
+          className="relative sm:scale-[104%] overflow-hidden object-cover"
           fill
         />
       </div>
