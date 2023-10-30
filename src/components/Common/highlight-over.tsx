564d7de99e01715bb18d72fea0dd8f2b0031bbf8
gxxrxn <rbfks1221@naver.com>
2023-10-31 03:22:09 +0900

    style: HighlightOver 컴포넌트 w-fit 속성 추가

--- src/components/Common/highlight-over.tsx
+++ src/components/Common/highlight-over.tsx
@@ -1,6 +1,6 @@
 const HighlightOver = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <span className="hover:bg-yellow-800 hover:text-black cursor-pointer">
+    <span className="hover:bg-yellow-800 hover:text-black cursor-pointer w-fit">
       {children}
     </span>
   );
