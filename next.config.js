f48c9d3ad14e3f0b148b702b050e7ffca1c1917a
gxxrxn <rbfks1221@naver.com>
2023-10-31 18:11:21 +0900

    feat: pwa 적용

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
@@ -25,4 +29,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,71 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: "Fryfolio",
   description: "developer kyurankim portfolio",
+  themeColor: "#ffc211",
+  icons: {
+    other: [
+      {
+        url: "/icons/splashscreens/iphone5_splash.png",
+        media:
+          "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/iphone6_splash.png",
+        media:
+          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/iphoneplus_splash.png",
+        media:
+          "(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/iphonex_splash.png",
+        media:
+          "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/iphonexr_splash.png",
+        media:
+          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/iphonexsmax_splash.png",
+        media:
+          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/ipad_splash.png",
+        media:
+          "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/ipadpro1_splash.png",
+        media:
+          "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/ipadpro3_splash.png",
+        media:
+          "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)",
+        rel: "apple-touch-startup-image",
+      },
+      {
+        url: "/icons/splashscreens/ipadpro2_splash.png",
+        media:
+          "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)",
+        rel: "apple-touch-startup-image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
